1831584e6d573d06f3b500d627524d7d2196179e
jdr0dn3y <justin@donnaruma.us>
2016-12-03 04:26:29 +0000

    Fix countdown timer

--- assets/js/plugins/event-timer.js
+++ assets/js/plugins/event-timer.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.event-timer').countdown(new Date(2017, 5-1, 13, 12, 00, 00), function(event) {
     jQuery(".event-timer__month").html(event.strftime('' + '%m'));
-    jQuery(".event-timer__days").html(event.strftime('' + '%d'));
+    jQuery(".event-timer__days").html(event.strftime('' + '%n'));
     jQuery(".event-timer__hours").html(event.strftime('' + '%H'));
     jQuery(".event-timer__minutes").html(event.strftime('' + '%M'));
     jQuery(".event-timer__seconds").html(event.strftime('' + '%S'));
